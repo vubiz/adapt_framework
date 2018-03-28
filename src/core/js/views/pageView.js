c36aa712b9df815e36df5845f15967a12ea417e7
Guy Willis <guy.willis@kineo.com>
2018-03-28 14:28:18 +0100

    Page refactor and indentation amends

--- src/core/js/views/pageView.js
+++ src/core/js/views/pageView.js
@@ -1,74 +1,76 @@
 define([
-    'core/js/adapt',
-    'core/js/views/adaptView',
-    'core/js/views/articleView'
+  'core/js/adapt',
+  'core/js/views/adaptView',
+  'core/js/views/articleView'
 ], function(Adapt, AdaptView, ArticleView) {
 
-    var PageView = AdaptView.extend({
-        
-        className: function() {
-            return "page " + 
-            this.model.get('_id') + 
-            " " + this.model.get('_classes') + 
-            " " + this.setVisibility() +
-            " " + (this.model.get('_isComplete') ? 'completed' : '');
-        },
+  var PageView = AdaptView.extend({
 
-        preRender: function() {
-            this.disableAnimation = Adapt.config.has('_disableAnimation') ? Adapt.config.get('_disableAnimation') : false;
-            this.$el.css('opacity', 0);
-            this.listenTo(this.model, 'change:_isReady', this.isReady);
+    className: function() {
+      return "page " +
+      this.model.get('_id') +
+      " " + this.model.get('_classes') +
+      " " + this.setVisibility() +
+      " " + (this.model.get('_isComplete') ? 'page--completed' : '');
+    },
 
-            var accessibility = Adapt.config.get('_accessibility');
-            if (!accessibility._isEnabled && !accessibility._isEnabledOnTouchDevices) {
-                return;
-            }
-            // create aria-label outside of #wrapper
-            this.$pageLabel = $('<div/>', {
-                'class': 'aria-label relative a11y-ignore-focus prevent-default',
-                tabindex: 0,
-                role: 'region',
-                text: Adapt.course.get('_globals')._accessibility._ariaLabels.pageEnd
-            }).appendTo('body');
-        },
+    preRender: function() {
+      this.disableAnimation = Adapt.config.has('_disableAnimation') ? Adapt.config.get('_disableAnimation') : false;
+      this.$el.css('opacity', 0);
+      this.listenTo(this.model, 'change:_isReady', this.isReady);
 
-        isReady: function() {
-            if (this.model.get('_isReady')) {
-                _.defer(_.bind(function() {
-                    $('.loading').hide();
-                    $(window).scrollTop(0);
-                    Adapt.trigger('pageView:ready', this);
-                    var styleOptions = { opacity: 1 };
-                    if (this.disableAnimation) {
-                        this.$el.css(styleOptions);
-                        $.inview();
-                    } else {
-                        this.$el.velocity(styleOptions, {
-                            duration: 'fast',
-                            complete: function() {
-                                $.inview();
-                            }
-                        });
-                    }
-                    $(window).scroll();
-                }, this));
-            }
-        },
+      var accessibility = Adapt.config.get('_accessibility');
+      if (!accessibility._isEnabled && !accessibility._isEnabledOnTouchDevices) {
+        return;
+      }
+      // create aria-label outside of #wrapper
+      this.$pageLabel = $('<div/>', {
+        'class': 'aria-label relative a11y-ignore-focus prevent-default',
+        tabindex: 0,
+        role: 'region',
+        text: Adapt.course.get('_globals')._accessibility._ariaLabels.pageEnd
+      }).appendTo('body');
+    },
 
-        remove: function() {
-            if (this.$pageLabel) {
-                this.$pageLabel.remove();
-            }
-            AdaptView.prototype.remove.call(this);
-        }
+    isReady: function() {
+      if (this.model.get('_isReady')) {
+        _.defer(_.bind(function() {
+          $('.loading').hide();
+          $(window).scrollTop(0);
+          Adapt.trigger('pageView:ready', this);
+          var styleOptions = { opacity: 1 };
 
-    }, {
-        childContainer: '.article-container',
-        childView: ArticleView,
-        type: 'page',
-        template: 'page'
-    });
+          if (this.disableAnimation) {
+            this.$el.css(styleOptions);
+            $.inview();
+          } else {
+            this.$el.velocity(styleOptions, {
+              duration: 'fast',
+              complete: function() {
+                $.inview();
+              }
+            });
+          }
 
-    return PageView;
+          $(window).scroll();
+        }, this));
+      }
+    },
+
+    remove: function() {
+      if (this.$pageLabel) {
+        this.$pageLabel.remove();
+      }
+      AdaptView.prototype.remove.call(this);
+    }
+
+  }, {
+    childContainer: '.article__container',
+    childView: ArticleView,
+    type: 'page',
+    template: 'page'
+  });
+
+  return PageView;
 
 });
