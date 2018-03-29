a04b38ad508d75c46e5ddeca59e57157a5c06f4b
Kirsty Hames <kirsty.hames@kirsty.local>
2018-03-29 10:41:14 +0100

    update notify js inline with master

--- src/core/js/views/notifyView.js
+++ src/core/js/views/notifyView.js
@@ -2,241 +2,246 @@ define([
     'core/js/adapt'
 ], function(Adapt) {
 
-  var NotifyView = Backbone.View.extend({
+    var NotifyView = Backbone.View.extend({
 
-    className: 'notify',
-    disableAnimation: false,
-    escapeKeyAttached: false,
+        className: function() {
+            var classes = 'notify ';
+            classes += (this.model.get('_classes') || '');
+            return classes;
+        },
 
-    initialize: function() {
-      this.disableAnimation = Adapt.config.has('_disableAnimation') ? Adapt.config.get('_disableAnimation') : false;
+        disableAnimation: false,
 
-      this.setupEventListeners();
+        escapeKeyAttached: false,
 
-      //include accessibility globals in notify model
-      this.model.set('_globals', Adapt.course.get('_globals'));
-      this.render();
-    },
+        initialize: function() {
+            this.disableAnimation = Adapt.config.has('_disableAnimation') ? Adapt.config.get('_disableAnimation') : false;
 
-    setupEventListeners: function() {
-      this.listenTo(Adapt, {
-        'remove page:scrollTo': this.closeNotify,
-        'notify:resize': this.resetNotifySize,
-        'notify:cancel': this.cancelNotify,
-        'notify:close': this.closeNotify,
-        'device:resize': this.resetNotifySize,
-        'accessibility:toggle': this.onAccessibilityToggle
-      });
+            this.setupEventListeners();
 
-      this._onKeyUp = _.bind(this.onKeyUp, this);
-      this.setupEscapeKey();
-    },
+            //include accessibility globals in notify model
+            this.model.set('_globals', Adapt.course.get('_globals'));
+            this.render();
+        },
 
-    setupEscapeKey: function() {
-      var hasAccessibility = Adapt.config.has('_accessibility') && Adapt.config.get('_accessibility')._isActive;
+        setupEventListeners: function() {
+            this.listenTo(Adapt, {
+                'remove page:scrollTo': this.closeNotify,
+                'notify:resize': this.resetNotifySize,
+                'notify:cancel': this.cancelNotify,
+                'notify:close': this.closeNotify,
+                'device:resize': this.resetNotifySize,
+                'accessibility:toggle': this.onAccessibilityToggle
+            });
 
-      if (!hasAccessibility && ! this.escapeKeyAttached) {
-        $(window).on('keyup', this._onKeyUp);
-        this.escapeKeyAttached = true;
-      } else {
-        $(window).off('keyup', this._onKeyUp);
-        this.escapeKeyAttached = false;
-      }
-    },
+            this._onKeyUp = _.bind(this.onKeyUp, this);
+            this.setupEscapeKey();
+        },
 
-    onAccessibilityToggle: function() {
-      this.setupEscapeKey();
-    },
+        setupEscapeKey: function() {
+            var hasAccessibility = Adapt.config.has('_accessibility') && Adapt.config.get('_accessibility')._isActive;
 
-    onKeyUp: function(event) {
-      if (event.which != 27) return;
-      event.preventDefault();
+            if (!hasAccessibility && ! this.escapeKeyAttached) {
+                $(window).on('keyup', this._onKeyUp);
+                this.escapeKeyAttached = true;
+            } else {
+                $(window).off('keyup', this._onKeyUp);
+                this.escapeKeyAttached = false;
+            }
+        },
 
-      this.cancelNotify();
-    },
+        onAccessibilityToggle: function() {
+            this.setupEscapeKey();
+        },
 
-    events: {
-      'click .js-notify-popup-btn-alert':'onAlertButtonClicked',
-      'click .js-notify-popup-btn-prompt': 'onPromptButtonClicked',
-      'click .js-notify-popup-close-btn': 'onCloseButtonClicked',
-      'click .js-notify-shadow-click': 'onShadowClicked'
-    },
+        onKeyUp: function(event) {
+            if (event.which != 27) return;
+            event.preventDefault();
 
-    render: function() {
-      var data = this.model.toJSON();
-      var template = Handlebars.templates['notify'];
+            this.cancelNotify();
+        },
 
-      //hide notify container
-      this.$el.addClass('u-visibility-hidden');
-      //attach popup + shadow
-      this.$el.html(template(data)).prependTo('body');
-      //hide popup
-      this.$('.notify__popup').addClass('u-visibility-hidden');
-      //show notify container
-      this.$el.removeClass('u-visibility-hidden');
+        events: {
+            'click .js-notify-popup-btn-alert':'onAlertButtonClicked',
+            'click .js-notify-popup-btn-prompt': 'onPromptButtonClicked',
+            'click .js-notify-popup-close-btn': 'onCloseButtonClicked',
+            'click .js-notify-shadow-click': 'onShadowClicked'
+        },
 
-      this.showNotify();
-      return this;
-    },
+        render: function() {
+            var data = this.model.toJSON();
+            var template = Handlebars.templates['notify'];
 
-    onAlertButtonClicked: function(event) {
-      event.preventDefault();
-      //tab index preservation, notify must close before subsequent callback is triggered
-      this.closeNotify();
-      Adapt.trigger(this.model.get('_callbackEvent'), this);
-    },
+            //hide notify container
+            this.$el.addClass('u-visibility-hidden');
+            //attach popup + shadow
+            this.$el.html(template(data)).prependTo('body');
+            //hide popup
+            this.$('.notify__popup').addClass('u-visibility-hidden');
+            //show notify container
+            this.$el.removeClass('u-visibility-hidden');
 
-    onPromptButtonClicked: function(event) {
-      event.preventDefault();
-      //tab index preservation, notify must close before subsequent callback is triggered
-      this.closeNotify();
-      Adapt.trigger($(event.currentTarget).attr('data-event'));
-    },
+            this.showNotify();
+            return this;
+        },
 
-    onCloseButtonClicked: function(event) {
-      event.preventDefault();
-      //tab index preservation, notify must close before subsequent callback is triggered
-      this.cancelNotify();
-    },
+        onAlertButtonClicked: function(event) {
+            event.preventDefault();
+            //tab index preservation, notify must close before subsequent callback is triggered
+            this.closeNotify();
+            Adapt.trigger(this.model.get('_callbackEvent'), this);
+        },
 
-    onShadowClicked: function(event) {
-      event.preventDefault();
-      this.cancelNotify();
-    },
+        onPromptButtonClicked: function(event) {
+            event.preventDefault();
+            //tab index preservation, notify must close before subsequent callback is triggered
+            this.closeNotify();
+            Adapt.trigger($(event.currentTarget).attr('data-event'));
+        },
 
-    cancelNotify: function() {
-      if (this.model.get("_isCancellable") === false) {
-          return;
-      }
-      //tab index preservation, notify must close before subsequent callback is triggered
-      this.closeNotify();
-      Adapt.trigger('notify:cancelled');
-    },
+        onCloseButtonClicked: function(event) {
+            event.preventDefault();
+            //tab index preservation, notify must close before subsequent callback is triggered
+            this.cancelNotify();
+        },
 
-    resetNotifySize: function() {
-      $('.notify__popup').removeAttr('style');
+        onShadowClicked: function(event) {
+            event.preventDefault();
+            if (this.model.get("_closeOnShadowClick") === false) return;
+            this.cancelNotify();
+        },
 
-      this.resizeNotify();
-    },
+        cancelNotify: function() {
+            if (this.model.get("_isCancellable") === false) return;
+            //tab index preservation, notify must close before subsequent callback is triggered
+            this.closeNotify();
+            Adapt.trigger('notify:cancelled');
+        },
 
-    resizeNotify: function() {
-      var windowHeight = $(window).height();
-      var notifyHeight = this.$('.notify__popup').outerHeight();
+        resetNotifySize: function() {
+            $('.notify__popup').removeAttr('style');
 
-      if (notifyHeight > windowHeight) {
-        this.$('.notify__popup').css({
-          'height': '100%',
-          'top': 0,
-          'overflow-y': 'scroll',
-          '-webkit-overflow-scrolling': 'touch'
-        });
-      } else {
-        this.$('.notify__popup').css({
-          'margin-top': -(notifyHeight/2)
-        });
-      }
-    },
+            this.resizeNotify();
+        },
 
-    showNotify: function() {
+        resizeNotify: function() {
+            var windowHeight = $(window).height();
+            var notifyHeight = this.$('.notify__popup').outerHeight();
 
-      this.addSubView();
+            if (notifyHeight > windowHeight) {
+                this.$('.notify__popup').css({
+                    'height':'100%',
+                    'top':0,
+                    'overflow-y': 'scroll',
+                    '-webkit-overflow-scrolling': 'touch'
+                });
+            } else {
+                this.$('.notify__popup').css({
+                    'margin-top': -(notifyHeight/2)
+                });
+            }
+        },
 
-      Adapt.trigger('notify:opened', this);
+        showNotify: function() {
 
-      this.$el.imageready( _.bind(loaded, this));
+            this.addSubView();
 
-      function loaded() {
-        if (this.disableAnimation) {
-          this.$('.notify__shadow').css('display', 'block');
-        } else {
+            Adapt.trigger('notify:opened', this);
 
-          this.$('.notify__shadow').velocity({ opacity: 0 }, {duration:0}).velocity({ opacity: 1 }, {duration:400, begin: _.bind(function() {
-              this.$('.notify__shadow').css('display', 'block');
-          }, this)});
+            this.$el.imageready( _.bind(loaded, this));
+
+            function loaded() {
+                if (this.disableAnimation) {
+                    this.$('.notify__shadow').css('display', 'block');
+                } else {
+
+                    this.$('.notify__shadow').velocity({ opacity: 0 }, {duration:0}).velocity({ opacity: 1 }, {duration:400, begin: _.bind(function() {
+                        this.$('.notify-shadow').css('display', 'block');
+                    }, this)});
+
+                }
+
+                this.resizeNotify();
+
+                if (this.disableAnimation) {
+
+                    this.$('.notify__popup').removeClass('u-visibility-hidden');
+                    complete.call(this);
+
+                } else {
+
+                    this.$('.notify__popup').velocity({ opacity: 0 }, {duration:0}).velocity({ opacity: 1 }, { duration:400, begin: _.bind(function() {
+                        this.$('.notify__popup').removeClass('u-visibility-hidden');
+                        complete.call(this);
+                    }, this) });
+
+                }
+
+                function complete() {
+                    //allows popup manager to control focus
+                    Adapt.trigger('popup:opened', this.$('.notify__popup'));
+                    $('body').scrollDisable();
+                    $('html').addClass('notify');
+
+                    //set focus to first accessible element
+                    this.$('.notify__popup').a11y_focus();
+                }
+            }
+
+        },
+
+        addSubView: function() {
+
+            this.subView = this.model.get("_view");
+            if (!this.subView) return;
+
+            this.$(".notify__popup-content-inner").append(this.subView.$el);
+
+        },
+
+        closeNotify: function (event) {
+
+            if (this.disableAnimation) {
+
+                this.$('.notify__popup').addClass('u-visibility-hidden');
+                this.$el.addClass('u-visibility-hidden');
+
+                this.remove();
+
+            } else {
+
+                this.$('.notify__popup').velocity({ opacity: 0 }, {duration:400, complete: _.bind(function() {
+                    this.$('.notify__popup').addClass('u-visibility-hidden');
+                }, this)});
+
+                this.$('.notify__shadow').velocity({ opacity: 0 }, {duration:400, complete:_.bind(function() {
+                    this.$el.addClass('u-visibility-hidden');
+                    this.remove();
+                }, this)});
+            }
+
+            $('body').scrollEnable();
+            $('html').removeClass('notify');
+
+            Adapt.trigger('popup:closed');
+            Adapt.trigger('notify:closed');
+        },
+
+        remove: function() {
+            this.removeSubView();
+            Backbone.View.prototype.remove.apply(this, arguments);
+        },
+
+        removeSubView: function() {
+
+            if (!this.subView) return;
+            this.subView.remove();
+            this.subView = null;
 
         }
 
-        this.resizeNotify();
+    });
 
-        if (this.disableAnimation) {
-
-          this.$('.notify__popup').removeClass('u-visibility-hidden');
-          complete.call(this);
-
-        } else {
-
-          this.$('.notify__popup').velocity({ opacity: 0 }, {duration:0}).velocity({ opacity: 1 }, { duration:400, begin: _.bind(function() {
-              this.$('.notify__popup').removeClass('u-visibility-hidden');
-              complete.call(this);
-          }, this) });
-
-        }
-
-        function complete() {
-          //allows popup manager to control focus
-          Adapt.trigger('popup:opened', this.$('.notify__popup'));
-          $('body').scrollDisable();
-          $('html').addClass('notify');
-
-          //set focus to first accessible element
-          this.$('.notify__popup').a11y_focus();
-        }
-      }
-
-    },
-
-    addSubView: function() {
-
-      this.subView = this.model.get("_view");
-      if (!this.subView) return;
-
-      this.$(".notify__popup-content-inner").append(this.subView.$el);
-
-    },
-
-    closeNotify: function (event) {
-
-      if (this.disableAnimation) {
-
-        this.$('.notify__popup').addClass('u-visibility-hidden');
-        this.$el.addClass('u-visibility-hidden');
-
-        this.remove();
-
-      } else {
-
-        this.$('.notify__popup').velocity({ opacity: 0 }, {duration:400, complete: _.bind(function() {
-          this.$('.notify__popup').addClass('u-visibility-hidden');
-        }, this)});
-
-        this.$('.notify__shadow').velocity({ opacity: 0 }, {duration:400, complete:_.bind(function() {
-          this.$el.addClass('u-visibility-hidden');
-          this.remove();
-        }, this)});
-      }
-
-      $('body').scrollEnable();
-      $('html').removeClass('notify');
-
-      Adapt.trigger('popup:closed');
-      Adapt.trigger('notify:closed');
-    },
-
-    remove: function() {
-      this.removeSubView();
-      Backbone.View.prototype.remove.apply(this, arguments);
-    },
-
-    removeSubView: function() {
-
-      if (!this.subView) return;
-      this.subView.remove();
-      this.subView = null;
-
-    }
-
-  });
-
-  return NotifyView;
+    return NotifyView;
 
 });
